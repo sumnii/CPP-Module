c8d2aca5a003b5979be12f78794efdbd4dc2ed9b
sumnii <sumsong@student.42seoul.kr>
2022-11-07 20:03:02 +0900

    [FEAT] cpp 00: ex01 main문 입력 받기

---
+++ cpp00/ex01/PhoneBook.h
@@ -0,0 +1,30 @@
+#ifndef CPP_MODULE_PHONEBOOK_H
+#define CPP_MODULE_PHONEBOOK_H
+
+#include "iostream"
+
+#define CMD_ELSE	0
+#define CMD_ADD		1
+#define CMD_SEARCH	2
+#define CMD_EXIT	3
+
+//class Contact {
+//private:
+//	std::string first_name;
+//	std::string last_name;
+//	std::string nickname;
+//	std::string phone;
+//	std::string secret;
+//public:
+//
+//};
+//
+//class PhoneBook {
+//private:
+//	Contact contacts[8];
+//	int save_cnt;
+//
+//public:
+//};
+
+#endif //CPP_MODULE_PHONEBOOK_H
---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,35 @@
+#include "PhoneBook.h"
+
+int cmd_type(std::string cmd){
+	if (cmd == "ADD")
+		return (CMD_ADD);
+	else if (cmd == "SEARCH")
+		return (CMD_SEARCH);
+	else if (cmd == "EXIT")
+		return (CMD_EXIT);
+	else
+	return (CMD_ELSE);
+}
+
+int main() {
+	std::string cmd;
+
+	while (true) {
+		std::cout << "명령을 입력하세요.\n1. ADD\n2. SEARCH\n3. EXIT\n";
+		std::cin >> cmd;
+		std::cout << "입력 커맨드 : " << cmd << std::endl;
+		switch (cmd_type(cmd)) {
+			case CMD_ADD:
+				std::cout << "add를 입력했습니다.\n";
+				break;
+			case CMD_SEARCH:
+				std::cout << "search를 입력\n";
+				break;
+			case CMD_EXIT:
+				std::cout << "exit를 입력\n";
+				exit(0);
+			case CMD_ELSE:
+				std::cout << "다시 ";
+		}
+	}
+}
