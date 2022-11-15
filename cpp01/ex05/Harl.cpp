16ebee2189311806e3bb7b44b8316b4d5a687f46
sumnii <sumsong@student.42seoul.kr>
2022-11-15 20:26:50 +0900

    [FEAT] implement Harl::complain() using pointers to member functions

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,21 +1,37 @@
 #include "Harl.h"
 
 void Harl::debug(void) {
-
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.";
+	std::cout << std::endl << "I really do!" << std::endl;
+	std::cout << std::endl;
 }
 
 void Harl::info(void) {
-
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
+	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << std::endl;
 }
 
 void Harl::warning(void) {
-
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month.";
+	std::cout << std::endl;
+	std::cout << std::endl;
 }
 
-void Harl::error(void){
-
+void Harl::error(void) {
+	std::cout << "This is unacceptable!" << std::endl;
+	std::cout << "I want to speak to the manager now." << std::endl;
+	std::cout << std::endl;
 }
 
 void Harl::complain(std::string level) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*f[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
+	for (int i = 0; i < 4; ++i) {
+		if (level == levels[i])
+			(this->*f[i])();
+	}
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,4 +1,14 @@
 #include "Harl.h"
 
-int main(int argc, char **argv) {
+int main(void) {
+	Harl harl;
+
+	harl.complain("debug");
+	harl.complain("DEBUG");
+	harl.complain("info");
+	harl.complain("INFO");
+	harl.complain("warning");
+	harl.complain("WARNING");
+	harl.complain("error");
+	harl.complain("ERROR");
 }
