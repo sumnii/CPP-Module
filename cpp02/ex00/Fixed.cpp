a1976509ae9f1f5dccc2cbc4907a707bc07d8b80
sumnii <sumsong@student.42seoul.kr>
2022-11-18 17:42:01 +0900

    [FEAT] setRawBits function

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -18,9 +18,13 @@ Fixed &Fixed::operator = (const Fixed &copy) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	this->num = copy.getRawBits();
 	return *this;
-}
+};
 
 int Fixed::getRawBits(void) const {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->num);
+};
+
+void Fixed::setRawBits(int const raw) {
+	this->num = raw;
 };
