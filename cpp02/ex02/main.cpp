c6fc6e0be613390fcf3b5101953c4baa730907ab
sumnii <sumsong@student.42seoul.kr>
2022-11-18 19:11:26 +0900

    [FEAT] add my test code

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -3,7 +3,20 @@
 int main(void) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
+	Fixed c(42);
+	Fixed d(42.0f);
+	Fixed e(42.1f);
 
+	std::cout << "===== my test =====" << std::endl;
+	std::cout << "c " << c << std::endl;
+	std::cout << "d " << d << std::endl;
+	std::cout << "e " << e << std::endl << std::endl;
+
+	std::cout << "c == d  " << std::boolalpha << (c==d) << std::endl;
+	std::cout << " d > e  " << std::boolalpha << (d>e) << std::endl;
+	std::cout << " d < e  " << std::boolalpha << (d<e) << std::endl << std::endl;
+
+	std::cout << "===== 42 test =====" << std::endl;
 	std::cout << a << std::endl;
 //	std::cout << ++a << std::endl;
 //	std::cout << a << std::endl;
