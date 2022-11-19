cf0e3c49254366bc63719cc5bf5dc78345f5d412
sumnii <sumsong@student.42seoul.kr>
2022-11-19 22:25:52 +0900

    [FEAT] main.cpp for test

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,5 +1,14 @@
 #include "ClapTrap.h"
 
 int main() {
+	ClapTrap one = ClapTrap("ONE");
+	ClapTrap two = ClapTrap("TWO");
 
+	one.attack("TWO");
+	two.takeDamage(5);
+	two.attack("ONE");
+	one.takeDamage(10);
+	one.attack("TWO");
+	one.beRepaired(5);
+	one.attack("TWO");
 }
