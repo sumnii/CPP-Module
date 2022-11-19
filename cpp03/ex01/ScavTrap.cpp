57f49650386c9d46f963b9dfe1ff6d16ac8cbb05
sumnii <sumsong@student.42seoul.kr>
2022-11-20 01:08:14 +0900

    [FEAT] implement ScavTrap constructors, destructor

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -1,2 +1,20 @@
 #include "ScavTrap.h"
 
+ScavTrap::ScavTrap() {
+	std::cout << "New ScavTrap " << name << "!" << std::endl;
+	hit_point = 100;
+	energy_point = 50;
+	attack_damage = 20;
+}
+
+ScavTrap::ScavTrap(std::string name) {
+	std::cout << "New ScavTrap " << name << "!" << std::endl;
+	this->name = name;
+	hit_point = 100;
+	energy_point = 50;
+	attack_damage = 20;
+}
+
+ScavTrap::~ScavTrap() {
+	std::cout << "Bye ScavTrap " << name << "!" << std::endl;
+}
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -1,26 +1,11 @@
-#include "ClapTrap.h"
+#include "ScavTrap.h"
 
 int main() {
 	ClapTrap tmp;
 	ClapTrap one = ClapTrap("ONE");
 	ClapTrap two = ClapTrap("TWO");
-	ClapTrap three = ClapTrap("THREE");
+	ScavTrap tmp2;
+	ScavTrap three = ScavTrap("THREE");
 
-	std::cout << std::endl;
-	one.attack("TWO");
-	two.takeDamage(5);
-	std::cout << std::endl;
-
-	two.attack("ONE");
-	one.takeDamage(10);
-	std::cout << std::endl;
-
-	one.attack("TWO");
-	one.beRepaired(5);
-	std::cout << std::endl;
-
-	two.beRepaired(5);
-	two.attack("THREE");
-	three.takeDamage(10);
 	std::cout << std::endl;
 }
