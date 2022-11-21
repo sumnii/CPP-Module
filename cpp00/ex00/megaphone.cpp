322eb4a5ce0eb51337b774a1b5c1a03d9953b571
sumnii <sumsong@student.42seoul.kr>
2022-11-21 16:44:11 +0900

    [CHORE] code formatting

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,8 +3,7 @@
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-		std::cout << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	} else {
 		for (int i = 1; i < argc; i++) {
 			std::string str;
