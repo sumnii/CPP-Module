289dab75d5e67e022800e0d1b2cedee701f31b3d
sumnii <sumsong@student.42seoul.kr>
2022-11-22 13:24:26 +0900

    [FEAT] edit main.cpp for clean test case

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -3,42 +3,63 @@
 #include "WrongCat.h"
 #include "unistd.h"
 
-int main() {
-	Animal *dog = new Dog();
-	Animal *cat = new Cat();
-	Animal *animals[4];
-
-	std::cout << std::endl;
-	for (size_t i = 0; i < (sizeof(animals) / sizeof(*animals)); ++i) {
-		if (i % 2 == 0)
-			animals[i] = new Dog();
-		else
-			animals[i] = new Cat();
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		std::cout << "TEST > ./animal [1 | 2 | 3]" << std::endl;
+		return 0;
 	}
-	std::cout << std::endl;
-	dog->getBrain().setIdeas(0, "original dog's brain");
-	std::cout << "original[0] : " << dog->getBrain().getIdeas(0) << std::endl;
-	Animal *deep_copy_dog = new Dog();
-	std::cout << "copy dog[0] : " << deep_copy_dog->getBrain().getIdeas(0) << std::endl;
-	*deep_copy_dog = *dog;
-	std::cout << "copy dog[0] : " << deep_copy_dog->getBrain().getIdeas(0) << std::endl;
-	std::cout << std::endl;
-	cat->getBrain().setIdeas(3, "original cat's brain");
-	std::cout << "original[3] : " << cat->getBrain().getIdeas(3) << std::endl;
-	Animal *deep_copy_cat = new Cat();
-	*deep_copy_cat = *cat;
-	std::cout << "copy cat[3] : " << deep_copy_cat->getBrain().getIdeas(3) << std::endl;
-	std::cout << std::endl;
+	if ((std::string) argv[1] == "1") {
+		const Animal *j = new Dog();
+		const Animal *i = new Cat();
 
-	delete(dog);
-	delete(deep_copy_dog);
-	delete(cat);
-	delete(deep_copy_cat);
-	std::cout << std::endl;
-	for (size_t i = 0; i < (sizeof(animals) / sizeof(*animals)); ++i)
-		delete animals[i];
-//	while (1) {
-//		system("leaks animal");
-//		sleep(10);
-//	}
+		std::cout << std::endl;
+		delete j;
+		delete i;
+	} else if ((std::string) argv[1] == "2") {
+		Animal *animals[4];
+
+		for (size_t i = 0; i < (sizeof(animals) / sizeof(*animals)); ++i) {
+			if (i % 2 == 0)
+				animals[i] = new Dog();
+			else
+				animals[i] = new Cat();
+		}
+		std::cout << std::endl;
+		for (size_t i = 0; i < (sizeof(animals) / sizeof(*animals)); ++i)
+			delete animals[i];
+	} else if ((std::string) argv[1] == "3") {
+		Dog *dog = new Dog();
+		Cat *cat = new Cat();
+
+		std::cout << std::endl;
+		dog->getBrain().setIdeas(0, "original dog's brain");
+		std::cout << "original[0] : " << dog->getBrain().getIdeas(0) << std::endl;
+		Animal *deep_copy_dog = new Dog();
+		std::cout << "copy dog[0] : " << deep_copy_dog->getBrain().getIdeas(0) << std::endl;
+		*deep_copy_dog = *dog;
+		std::cout << "copy dog[0] : " << deep_copy_dog->getBrain().getIdeas(0) << std::endl;
+
+		std::cout << std::endl;
+		std::cout << std::endl;
+		cat->getBrain().setIdeas(3, "original cat's brain");
+		std::cout << "original[3] : " << cat->getBrain().getIdeas(3) << std::endl;
+		Animal *deep_copy_cat = new Cat();
+		*deep_copy_cat = *cat;
+		std::cout << "copy cat[3] : " << deep_copy_cat->getBrain().getIdeas(3) << std::endl;
+		deep_copy_cat->getBrain().setIdeas(3, "copy cat's brain");
+		std::cout << "copy cat[3] : " << deep_copy_cat->getBrain().getIdeas(3) << std::endl;
+		std::cout << "original[3] : " << cat->getBrain().getIdeas(3) << std::endl;
+		std::cout << std::endl;
+		std::cout << std::endl;
+
+		delete(dog);
+		delete(deep_copy_dog);
+		delete(cat);
+		delete(deep_copy_cat);
+	}
+	while (1) {
+		system("leaks animal");
+		break;
+	}
+	return 0;
 }
