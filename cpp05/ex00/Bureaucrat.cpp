f6ebaee9e4b8641ea07e4a7d27a655439325e01d
sumnii <sumsong@student.42seoul.kr>
2022-11-22 23:39:08 +0900

    [FEAT] make a structure of Bureaucrat class

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,1 +1,21 @@
 #include "Bureaucrat.h"
+
+Bureaucrat::Bureaucrat() : name("Unknown"), grade(1) {}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : name(name), grade(grade) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &ref) : name(ref.name), grade(grade) {}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &ref) {
+	grade = ref.grade;
+}
+
+const std::string &Bureaucrat::getName() const {
+	return name;
+}
+
+int Bureaucrat::getGrade() const {
+	return grade;
+}
--- cpp05/ex00/Bureaucrat.h
+++ cpp05/ex00/Bureaucrat.h
@@ -1,9 +1,29 @@
 #ifndef EX00_BUREAUCRAT_H
 #define EX00_BUREAUCRAT_H
 
+#include "iostream"
 
 class Bureaucrat {
+private:
+	const std::string name;
+	int grade;
 
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+	~Bureaucrat();
+	Bureaucrat(const Bureaucrat &ref);
+	Bureaucrat &operator=(const Bureaucrat &ref);
+
+	const std::string &getName() const;
+	int getGrade() const;
+
+	class GradeTooHighException : public std::exception {
+		const char *what() const;
+	};
+	class GradeTooLowException  : public std::exception {
+		const char *what() const;
+	};
 };
 
 
