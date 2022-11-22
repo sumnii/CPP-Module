04ca204ad1e8cf1da373c2cd19ebb87837c9f94d
sumnii <sumsong@student.42seoul.kr>
2022-11-22 23:45:02 +0900

    [FEAT] add exception condition and throw

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -2,13 +2,29 @@
 
 Bureaucrat::Bureaucrat() : name("Unknown"), grade(1) {}
 
-Bureaucrat::Bureaucrat(const std::string &name, int grade) : name(name), grade(grade) {}
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : name(name) {
+	if (grade < 1)
+		throw GradeTooLowException();
+	else if (grade > 150)
+		throw GradeTooHighException();
+	this->grade = grade;
+}
 
 Bureaucrat::~Bureaucrat() {}
 
-Bureaucrat::Bureaucrat(const Bureaucrat &ref) : name(ref.name), grade(grade) {}
+Bureaucrat::Bureaucrat(const Bureaucrat &ref) : name(ref.name) {
+	if (grade < 1)
+		throw GradeTooLowException();
+	else if (grade > 150)
+		throw GradeTooHighException();
+	grade = ref.grade;
+}
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &ref) {
+	if (ref.grade < 1)
+		throw GradeTooLowException();
+	else if (ref.grade > 150)
+		throw GradeTooHighException();
 	grade = ref.grade;
 }
 
@@ -18,4 +34,12 @@ const std::string &Bureaucrat::getName() const {
 
 int Bureaucrat::getGrade() const {
 	return grade;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const {
+	return ("Grade is too High!\n");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const {
+	return ("Grade is too Low!\n");
 }
