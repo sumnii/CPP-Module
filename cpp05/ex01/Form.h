aa8ad2afb4b117cde4ea8a4307f516d6ac619fe9
sumnii <sumsong@student.42seoul.kr>
2022-11-24 17:17:44 +0900

    [FEAT] make a structure of Form class

---
+++ cpp05/ex01/Form.h
@@ -0,0 +1,34 @@
+#ifndef EX01_FORM_H
+#define EX01_FORM_H
+
+#include "Bureaucrat.h"
+
+class Form {
+private:
+	const std::string	name;
+	bool				is_signed;
+	const int 			access_to_sign;
+	const int 			access_to_execute;
+
+public:
+	const std::string getName() const;
+	bool getIsSigned() const;
+	const int getAccessToSign() const;
+	const int getAccessToExecute() const;
+
+	void beSigned(Bureaucrat &bur);
+
+	class GradeTooHighException : std::exception {
+	public:
+		const char *what() const throw();
+	};
+
+	class GradeTooLowException : std::exception {
+	public:
+		const char *what() const throw();
+	};
+};
+
+std::ostream &operator<<(std::ostream &out, Form &f);
+
+#endif //EX01_FORM_H
