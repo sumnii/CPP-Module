90d3ddfd86bbf2b23b5d82b5adacea101b646ab8
sumnii <sumsong@student.42seoul.kr>
2022-11-24 14:21:46 +0900

    [STYLE] print levels also

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -13,12 +13,14 @@
 #include "Harl.h"
 
 void Harl::debug(void) {
+	std::cout << " ~ DEBUG ~" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.";
 	std::cout << std::endl << "I really do!" << std::endl;
 	std::cout << std::endl;
 }
 
 void Harl::info(void) {
+	std::cout << " ~ INFO ~" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
 	std::cout << "You didn’t put enough bacon in my burger!" << std::endl;
 	std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;
@@ -26,6 +28,7 @@ void Harl::info(void) {
 }
 
 void Harl::warning(void) {
+	std::cout << " ~ WARNING ~" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
 	std::cout << "I’ve been coming for years whereas you started working here since last month.";
 	std::cout << std::endl;
@@ -33,6 +36,7 @@ void Harl::warning(void) {
 }
 
 void Harl::error(void) {
+	std::cout << " ~ ERROR ~" << std::endl;
 	std::cout << "This is unacceptable!" << std::endl;
 	std::cout << "I want to speak to the manager now." << std::endl;
 	std::cout << std::endl;
