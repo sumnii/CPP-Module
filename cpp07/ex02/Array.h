5b1f02fd1c164ca9f38abde37c39f4513b0ab53e
sumnii <sumsong@student.42seoul.kr>
2022-12-13 05:03:08 +0900

    [FEAT] overload operator[] of Array class and exception

--- cpp07/ex02/Array.h
+++ cpp07/ex02/Array.h
@@ -1,35 +1,53 @@
 #ifndef EX02_ARRAY_H
 #define EX02_ARRAY_H
 
-#include "iostream"
-
-template <typename T>
+template<typename T>
 class Array {
 private:
 	T *array;
+	std::size_t size;
+
+	class outOfIndexException : public std::exception {
+		const char *what() const throw() {
+			return ("Index is out of bounds!");
+		}
+	};
 
 public:
-	Array() : array(NULL) {};
-	Array(unsigned int n) { array = new T[n]; };
-	~Array() { delete[] array; };
-	Array(const Array &ref) {
-		size_t n = sizeof(ref.array) / sizeof(T);
+	Array() {};
 
-		array = new T[n];
-		for (size_t i = 0; i < n; ++i) {
+	Array(unsigned int n) : size(n) { array = new T[n]; };
+
+	~Array() { delete[] array; };
+
+	Array(const Array &ref) {
+		array = new T[ref.size];
+		size = ref.size;
+		for (size_t i = 0; i < ref.size; ++i)
 			array[i] = ref.array[i];
-		}
 	}
+
 	Array &operator=(const Array &ref) {
-		if (*this == ref)
+		if (this == &ref)
 			return (*this);
 		delete[] this->array;
-		size_t n = sizeof(ref.array) / sizeof(T);
-		array = new T[n];
-		for (size_t i = 0; i < n; ++i) {
+		array = new T[ref.size];
+		size = ref.size;
+		for (size_t i = 0; i < ref.size; ++i)
 			array[i] = ref.array[i];
-		}
 		return (*this);
+	}
+
+	T &operator[](std::size_t i) {
+		if (i > size)
+			throw outOfIndexException();
+		return (array[i]);
+	}
+
+	const T &operator[](const std::size_t i) const {
+		if (i > size)
+			throw outOfIndexException();
+		return (array[i]);
 	}
 };
 
