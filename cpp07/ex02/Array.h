f514f82a787351436f0d03495f1a49a852c28ec7
sumnii <sumsong@student.42seoul.kr>
2022-12-13 02:45:09 +0900

    [FEAT] implement constructors, destructor

--- cpp07/ex02/Array.h
+++ cpp07/ex02/Array.h
@@ -1,9 +1,36 @@
 #ifndef EX02_ARRAY_H
 #define EX02_ARRAY_H
 
+#include "iostream"
+
 template <typename T>
 class Array {
+private:
+	T *array;
 
+public:
+	Array() : array(NULL) {};
+	Array(unsigned int n) { array = new T[n]; };
+	~Array() { delete[] array; };
+	Array(const Array &ref) {
+		size_t n = sizeof(ref.array) / sizeof(T);
+
+		array = new T[n];
+		for (size_t i = 0; i < n; ++i) {
+			array[i] = ref.array[i];
+		}
+	}
+	Array &operator=(const Array &ref) {
+		if (*this == ref)
+			return (*this);
+		delete[] this->array;
+		size_t n = sizeof(ref.array) / sizeof(T);
+		array = new T[n];
+		for (size_t i = 0; i < n; ++i) {
+			array[i] = ref.array[i];
+		}
+		return (*this);
+	}
 };
 
 #endif //EX02_ARRAY_H
