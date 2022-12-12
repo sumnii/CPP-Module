a386d29b2eaba017b9817ddb61ddb9f3d0f5bc6c
sumnii <sumsong@student.42seoul.kr>
2022-12-13 00:22:13 +0900

    [FEAT] implement identify(Base &p)

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -23,14 +23,29 @@ void identify(Base* p) {
 		std::cout << "C" << std::endl;
 }
 
-//void identify(Base& p) {
-//
-//}
+void identify(Base& p) {
+	try {
+		A &a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+		static_cast<void>(a);
+	} catch (std::bad_cast) {}
+	try {
+		B &b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+		static_cast<void>(b);
+	} catch (std::bad_cast) {}
+	try {
+		C &c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+		static_cast<void>(c);
+	} catch (std::bad_cast) {}
+}
 
 
 int main() {
 	Base *rand = generate();
 
 	identify(rand);
+	identify(*rand);
 	delete rand;
 }
