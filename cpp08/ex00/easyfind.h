765832f48bdc6456c587b13982edce7fb0b958bc
sumnii <sumsong@student.42seoul.kr>
2022-12-13 17:16:47 +0900

    [FEAT] implement iterator of container type T

--- cpp08/ex00/easyfind.h
+++ cpp08/ex00/easyfind.h
@@ -1,9 +1,18 @@
 #ifndef EX00_EASYFIND_H
 #define EX00_EASYFIND_H
 
-template <typename T>
-typename T::iterator easyfind(T container, int i) {
+#include "algorithm"
 
+template<typename T>
+typename T::iterator easyfind(T &container, int i) {
+	typename T::iterator itr;
+
+	if (container.begin() == container.end())
+		throw 0;
+	itr = find(container.begin(), container.end(), i);
+	if (itr == container.end())
+		throw 1;
+	return itr;
 }
 
 #endif //EX00_EASYFIND_H
