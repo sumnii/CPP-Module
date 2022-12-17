5a387d1f37d347a20ce31c6d325397978f830cec
sumnii <sumsong@student.42seoul.kr>
2022-12-17 17:43:16 +0900

    [TEST] add a test code with a bunch of numbers

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,32 +1,42 @@
 #include "Span.h"
 
-void leaks() {
-	std::cout << std::endl << "-------------------[ leaks test ]-------------------" << std::endl;
-	system("leaks iterator"); }
-
 int main()
 {
-	Span sp = Span(5);
+	{ // 5개 공간의 Span 클래스
+		Span sp = Span(5);
 
-	try {
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		// N개의 요소가 이미 찼음
+		try {
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			// N개의 요소가 이미 찼음
 //		sp.addNumber(10);
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+
+		try {
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
 	}
 
-	try {
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-	} catch (std::exception &e) {
-		std::cerr << e.what() << std::endl;
+	{ // 10000개 공간의 Span 클래스
+		Span sp = Span(10000);
+
+		sp.addNumberBunch();
+
+		try {
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		} catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
 	}
 
-//	atexit(leaks);
 	return 0;
 }
