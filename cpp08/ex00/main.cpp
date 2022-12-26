81fd738313e76c3d3df7bd9313c44cbccd059e1e
sumnii <sumsong@student.42seoul.kr>
2022-12-26 14:13:02 +0900

    [TEST] add case of list, deque

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,37 +1,47 @@
 #include "easyfind.h"
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 
 void leaks() {
 	std::cout << std::endl << "-------------------[ leaks test ]-------------------" << std::endl;
 	system("leaks iterator"); }
 
 int main() {
-	std::vector<int> vec;
+	std::vector<int> con;
+//	std::list<int> con;
+//	std::deque<int> con;
 
 	// 주석처리 시 : 빈 컨테이너 에러 케이스
-	vec.push_back(4);
-	vec.push_back(1);
-	vec.push_back(3);
-	vec.push_back(2);
+	con.push_back(4);
+	con.push_back(1);
+	con.push_back(3);
+	con.push_back(2);
 
 	std::vector<int>::iterator itr;
+//	std::list<int>::iterator itr;
+//	std::deque<int>::iterator itr;
 	try {
-		itr = easyfind(vec, 3);
-		std::cout << *itr << std::endl;
+		int i = 3;
+		itr = easyfind(con, i);
+		std::cout << *itr << "는 " << std::distance(con.begin(), itr) << "번 인덱스에 있습니다." << std::endl;
 	} catch (int err) {
-		std::cerr << " > easyfind(vec, 3) < " << std::endl;
+		std::cerr << " > easyfind(con, 3) < " << std::endl;
 		if (err == 0)
 			std::cerr << "빈 컨테이너입니다." << std::endl;
 		else if (err == 1)
 			std::cerr << "찾는 항목이 컨테이너에 없습니다." << std::endl;
 	}
 
+	std::cout << std::endl;
+
 	try {
-		itr = easyfind(vec, 5);
-		std::cout << *itr << std::endl;
+		int i = 5;
+		itr = easyfind(con, i);
+		std::cout << *itr << "는 " << std::distance(con.begin(), itr) << "번 인덱스에 있습니다." << std::endl;
 	} catch (int err) {
-		std::cerr << " > easyfind(vec, 5) < " << std::endl;
+		std::cerr << " > easyfind(con, 5) < " << std::endl;
 		if (err == 0)
 			std::cerr << "빈 컨테이너입니다." << std::endl;
 		else if (err == 1)
