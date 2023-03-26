74b78934f4854cafb2b466f5c624c2c1a5c08e30
sumnii <sumsong@student.42seoul.kr>
2023-03-26 22:45:42 +0900

    [REFACT] private func

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -11,25 +11,26 @@ class BitcoinExchange {
 private:
 	std::map<std::string, float> _exchangeData;
 
+	void setExchangeData(const std::string &key, float value);
+	float getExchangeRate(const std::string &key);
+	float getClosestDateDate(std::string &key);
+
+	void saveDataAfterParse(std::string &line);
+
+	void multiplyBitcoinAfterParse(std::string &line);
+	bool isValidateDate(std::string &date);
+	std::string getYear(std::string &date);
+	std::string getMonth(std::string &date);
+	std::string getDay(std::string &date);
+
 public:
 	BitcoinExchange();
 	~BitcoinExchange();
 	BitcoinExchange(const BitcoinExchange &ref);
 	BitcoinExchange &operator=(const BitcoinExchange &ref);
 
-	void setExchangeData(const std::string &key, float value);
-	float getExchangeRate(const std::string &key);
-	float getClosestDateDate(std::string &key);
-
 	void saveExchangeData();
-	void saveDataAfterParse(std::string &line);
-
 	void readBitcoinData(char *fileName);
-	void multiplyBitcoinAfterParse(std::string &line);
-	bool isValidateDate(std::string &date);
-	std::string getYear(std::string &date);
-	std::string getMonth(std::string &date);
-	std::string getDay(std::string &date);
 };
 
 
